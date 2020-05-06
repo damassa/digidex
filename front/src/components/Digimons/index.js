7b9006f42cdfa27722b186c68482cf1ea06c55a1
cristian <contato@cristiancunha.me>
2020-05-06 20:18:52 -0300

    Remake

--- front/src/components/Digimons/index.js
+++ front/src/components/Digimons/index.js
@@ -2,15 +2,18 @@ import React from 'react';
 import './digimonStyles.css';
 
 const Digimons = props => {
-    const {name, img} = props;
+    const {name, img, level} = props;
   
     return(
         <div className="card-container">
           <div className="card">
-            <div className="nome">{name}</div>
-              <div className="imagem">
-                <img src={img} alt={name} title={name} />
-              </div>
+            <div className="imagem">
+              <img src={img} alt={name} title={name} />
+            </div>
+            <div className="infos">
+              <div className="nome">Nome: {name}</div>
+              <div className="level">Level: {level}</div>
+            </div>
           </div>
         </div>
     );
--- front/src/components/Header/index.js
+++ front/src/components/Header/index.js
@@ -5,9 +5,6 @@ import logo from '../../assets/digimon-logo.png';
 const Header = () => {
     return(
         <header className="header">
-            <div className="logo-container">
-                <img src={logo} alt="Digimon" title="Digidex - A pokédex, for Digimon!"/>
-            </div>
         </header>
     );
 }
